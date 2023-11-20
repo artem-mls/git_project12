645378b3c86386c46ca679665413bbe930b39122
Малышев <malyshev.ao@oksei.ru>
2023-11-20 16:30:07 +0500

    Начинаем занятие 2 по GIT

--- program.py
+++ program.py
@@ -1,9 +1,13 @@
-from func import x2
+def my_superfunction():
+    print('What an awesome print!')
+
 
 def main():
     print('My first git program')
     print('And I change it every day')
-    print(x2(7))
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
